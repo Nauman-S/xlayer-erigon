8e22d5a9daa72a7385cb1425dddfe42af0185b64
Niven <sieniven@gmail.com>
2025-05-06 10:00:30 +0800

    Fix

--- zk/datastream/client/stream_client.go
+++ zk/datastream/client/stream_client.go
@@ -704,7 +704,7 @@ LOOP:
 		}
 
 		// Reach the end of range
-		if entryNum == toEntry || c.header.TotalEntries < entryNum+1 {
+		if entryNum == toEntry || c.header.TotalEntries < entryNum {
 			log.Trace("[Datastream client] reached the current end of the stream", "header_totalEntries", c.header.TotalEntries, "entryNum", entryNum)
 
 			if err := c.TrySendStopSignal(); err != nil {
