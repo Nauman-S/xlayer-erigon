b655509f116ade46268bdeb625821af66c0c5828
Nauman-S <nauman843@hotmail.com>
2025-05-07 18:04:37 +0800

    wrk-testing

--- tests/stress-testing/scripts/zkevm/getProverInput.lua
+++ tests/stress-testing/scripts/zkevm/getProverInput.lua
@@ -17,7 +17,8 @@ setup = function(thread)
 end
 
 request = function()
-    local body = string.format('{"jsonrpc":"2.0","method":"%s","params":[],"id":1}', methodName)
+    local batch = math.random(1, 2605)
+    local body = string.format('{"jsonrpc":"2.0","method":"%s","params":[%d,"full",true],"id":1}', methodName, batch)
     headers = {}
     headers["Content-Type"] = "application/json"
     return wrk.format("POST", nil, headers, body)
